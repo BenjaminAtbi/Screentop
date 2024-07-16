91dfc67382ba209ec7881e8018cada68d1fa9229
benjaminatbi <Benjaminatbi@gmail.com>
2024-07-15 23:25:37 -0700

    added tracked files

---
+++ Screentop/TableVM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Screentop
+{
+    internal class TableVM
+    {
+        public ObservableCollection<UIElement> Elements { get; }
+
+        public TableVM() 
+        {
+            Elements = new ObservableCollection<UIElement>();
+            
+            Button button = new Button();
+            button.Content = "clickables";
+            button.Width = 100;
+            button.Height = 100;
+
+            addElement(button, 50, 100);
+        }
+
+        private void addElement(UIElement element, int x, int y)
+        {
+            Canvas.SetTop(element, y);
+            Canvas.SetLeft(element, x);
+            Elements.Add(element);
+        }
+
+
+
+    }
+}
